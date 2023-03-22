1422e1b5e7fed301da588acbcc3e15b51b270781
blickwinkle <1095400631@qq.com>
2023-03-22 14:12:18 +0800

    proj0

--- src/planner/plan_func_call.cpp
+++ src/planner/plan_func_call.cpp
@@ -37,9 +37,20 @@ auto Planner::PlanFuncCall(const BoundFuncCall &expr, const std::vector<Abstract
 auto Planner::GetFuncCallFromFactory(const std::string &func_name, std::vector<AbstractExpressionRef> args)
     -> AbstractExpressionRef {
   // 1. check if the parsed function name is "lower" or "upper".
+  if (!(func_name == "lower" || func_name == "upper")) {
+    return {};
+  }
   // 2. verify the number of args (should be 1), refer to the test cases for when you should throw an `Excepetion`.
+  if (args.size() != 1) {
+    throw Exception(fmt::format("args number %d error", args.size()));
+  }
   // 3. return a `StringExpression` std::shared_ptr.
-  throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
+  if (func_name == "lower") {
+    return std::make_shared<StringExpression>(StringExpression(std::move(args[0]), StringExpressionType::Lower));
+  }
+  
+  return std::make_shared<StringExpression>(StringExpression(std::move(args[0]), StringExpressionType::Upper));
+  //throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
 }
 
 }  // namespace bustub
--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -1,5 +1,6 @@
 #include "primer/trie_store.h"
 #include "common/exception.h"
+#include "primer/trie.h"
 
 namespace bustub {
 
@@ -8,23 +9,60 @@ auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
   // Pseudo-code:
   // (1) Take the root lock, get the root, and release the root lock. Don't lookup the value in the
   //     trie while holding the root lock.
+  this->root_lock_.lock();
+  Trie root = this->root_;
+  this->root_lock_.unlock();
   // (2) Lookup the value in the trie.
+  auto ret_val = root.Get<T>(key);
   // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
   //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+  if (ret_val == nullptr) {
+    return std::nullopt;
+  }   
+  return ValueGuard<T>(std::move(root), *ret_val);
+ 
+  //throw NotImplementedException("TrieStore::Get is not implemented.");
 }
 
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+  this->write_lock_.lock();
+
+  this->root_lock_.lock();
+  Trie root = this->root_;
+  this->root_lock_.unlock();
+
+  Trie new_root = root.Put(key, std::move(value));
+
+  this->root_lock_.lock();
+  this->root_ = new_root;
+  this->root_lock_.unlock();
+
+  this->write_lock_.unlock();
+
+  //throw NotImplementedException("TrieStore::Put is not implemented.");
 }
 
 void TrieStore::Remove(std::string_view key) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  //throw NotImplementedException("TrieStore::Remove is not implemented.");
+
+  this->write_lock_.lock();
+
+  this->root_lock_.lock();
+  Trie root = this->root_;
+  this->root_lock_.unlock();
+
+  Trie new_root = root.Remove(key);
+
+  this->root_lock_.lock();
+  this->root_ = new_root;
+  this->root_lock_.unlock();
+
+  this->write_lock_.unlock();
 }
 
 // Below are explicit instantiation of template functions.
